54a406a3c38a05e54512883bfd4929b3b2e2168d
ETTARAK <ettarak.zouhair@gmail.com>
2021-04-02 11:17:25 +0100

    Coding, create file configuration to Database

---
+++ src/config.php
@@ -0,0 +1,16 @@
+
+
+<?php
+    
+    /* File content configuration  to Database and user access */
+
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "chatapp";
+
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  if(!$conn){
+    echo "Database connection error".mysqli_connect_error();
+  }
+?>
